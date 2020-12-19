203143040a07c10dcbea38e2b562c13fef043769
Ashish Subedi <ashishsubedi10@gmail.com>
2020-12-19 19:52:36 +0545

    Post UI Component

--- app/bronimal/src/components/ui/Post.jsx
+++ app/bronimal/src/components/ui/Post.jsx
@@ -1,36 +1,48 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import { IoPawOutline,IoChatboxOutline,IoShareSocialOutline } from "react-icons/io5";
 import './styles.css';
+import placeholder from './placeholder.jpg' ;
 
 export default function Post(props) {
 
     return (
-        <Card className='card-body' >
+        <Card className='card-main' >
             <Card.Header className='card-header'>
-                <Card.Img variant="left" src="holder.js/100px180" />
+                <Card.Img variant="left" className='avatar' src={placeholder}/>
                 <Card.Text className='card-header-title'>Card Title</Card.Text>
             </Card.Header>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-            <LikeToolbar/>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+            <Card.Img variant="top" className='card-image' src={placeholder} />
+            <LikeToolbar />
+            <Card.Body className='card-main-body '>
+                <Card.Text style={{fontWeight: 'bolder', display: 'inline'}}> Some User </Card.Text> 
+                <Card.Text style={{ display: 'inline'}}> Commented This </Card.Text> 
             </Card.Body>
         </Card>
     )
 
 }
 
-function LikeToolbar(props){
+function LikeToolbar(props) {
     return (
-        <Card className='card-toolbar'>
-            <Card.Img variant="top" src="holder.js/100px180" />
-          
-        </Card>
+        <div >
+            <div className='card-toolbar '>
+
+                <Button variant="light" className="card-toolbar-btn"><IoPawOutline className='card-toolbar-text' /></Button>
+                <Button variant="light" className="card-toolbar-btn"><IoChatboxOutline className='card-toolbar-text'/></Button>
+                <Button variant="light" className="card-toolbar-btn"><IoShareSocialOutline className='card-toolbar-text'/></Button>
+
+            </div>
+            <div className='card-toolbar-status'>
+              <Card.Img variant="left" className='avatar' src={placeholder} />
+
+                <Card.Text className='card-toolbar-status-text'>
+                    XYZ and 53 other liked
+                </Card.Text>
+            </div>
+
+        </div>
+
     )
 }
